5043851f77ec6e9e0a9a7cccc43e2bd0a4ea6ab8
Minh Tu <minhtu12394@gmail.com>
2018-07-02 16:39:26 +0700

    init model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  validates :name, presence: true, 
+    length: {maximum: Settings.name_maximum_length}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+    length: {maximum: Settings.email_maximum_length}, 
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :user_groups
+  has_many :groups, through: :user_groups
+  
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
