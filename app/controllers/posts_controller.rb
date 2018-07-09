875185a24b8e37337472e917f1a0c1aa15efc6db
Minh Tu <minhtu12394@gmail.com>
2018-07-09 07:28:23 +0700

    [user] delete their post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_post, only: %i(show)
+  before_action :load_post, only: %i(show destroy)
  	
   def create
     @post = current_user.posts.build post_params
@@ -12,7 +12,16 @@ class PostsController < ApplicationController
       redirect_to root_url
   end
 
-  def show;end
+  def show; end
+
+  def destroy
+    if post.destroy
+      respond_to do |format|
+        format.html {redirect_to root_url}
+        format.js    
+      end
+    end
+  end
 
   private
 
@@ -25,8 +34,8 @@ class PostsController < ApplicationController
   def load_post
     @post = Post.find_by id: params[:id]
 
-    return if @post
-    flash[:danger] = t "messages.not_found"
+    return if post
+    flash[:danger] = t "messages.not_found" + params[:id]
     redirect_to root_url 
   end
 end
